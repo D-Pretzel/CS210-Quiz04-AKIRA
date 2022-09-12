6b3a846a192207e1f40e8d36c878a172b1e2c9ec
David Petzold <C25David.Petzold@afacademy.af.edu>
2022-09-12 14:11:15 -0600

    It works!

--- quiz04-01.c
+++ quiz04-01.c
@@ -1,12 +1,33 @@
 /** quiz04-01.c
 * ===========================================================
-*    Name: 
-* Section:
+*    Name: David Petzold
+* Section: T6/7
 * Purpose: Quiz 4
 * ===========================================================
 */
 
+#include <stdio.h>
+#include <string.h>
+
+#define MAX 100
+
 int main() {
+    char food[MAX];
+    char color[MAX];
+
+    printf("Enter your favorite food: ");
+    scanf("%s", food);
+
+    printf("Enter your favorite color: ");
+    scanf("%s", color);
+
+    printf("Your favorite food is %s, your favorite color is %s.\n", food, color);
+
+    if (strcmp(food, "pizza") == 0) {
+        printf("Your favorite food is pizza.\n");
+    } else {
+        printf("Your favorite food is not pizza.\n");
+    }
 
     return 0;
 }
--- quiz04-02.c
+++ quiz04-02.c
@@ -1,12 +1,24 @@
 /** quiz04-02.c
 * ===========================================================
-*    Name: 
-* Section:
+*    Name: David Petzold
+* Section: T6/7
 * Purpose: Quiz 4
 * ===========================================================
 */
 
+#include <stdio.h>
+#include <string.h>
+
+#define MAX 100
+
 int main() {
+    char name[MAX];
+
+    printf("Enter your name: ");
+    scanf("%s", name);
+
+    printf("Your name, %s, is %li characters long and starts with the letter %c\n", name, strnlen(name, MAX), name[0]);
+
 
     return 0;
 }
